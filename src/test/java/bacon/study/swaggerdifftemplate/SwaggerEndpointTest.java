0551b2872fe1c216e9c73ad6f19548c7cf57376e
HJ Park <b2aconnn@gmail.com>
2026-05-14 19:19:03 +0900

    feat: add springdoc-openapi and verify /v3/api-docs endpoint

---
+++ src/test/java/bacon/study/swaggerdifftemplate/SwaggerEndpointTest.java
@@ -0,0 +1,25 @@
+package bacon.study.swaggerdifftemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class SwaggerEndpointTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void apiDocsReturns200() throws Exception {
+        mockMvc.perform(get("/v3/api-docs"))
+               .andExpect(status().isOk())
+               .andExpect(content().contentType("application/json"));
+    }
+}
